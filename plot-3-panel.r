22066c32b9067bd968dbb526fa9e5d6329038503
Telmo Nunes <pinanunes@gmail.com>
2020-04-04 22:07:06 +0100

    contempla diferente N de paises em base.R

--- plot-3-panel.r
+++ plot-3-panel.r
@@ -26,7 +26,7 @@ make_three_pannel_plot <- function(){
                                  stringsAsFactors = FALSE)
   covariates <- data_interventions[1:11, c(1,2,3,4,5,6, 7, 8)]
   data_summary<-data.frame()
-  for(i in 1:11){
+  for(i in 1:length(countries)){
     print(i)
     N <- length(dates[[i]])
     country <- countries[[i]]
